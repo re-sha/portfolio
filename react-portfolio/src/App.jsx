545fa4a1fe7b04e3cec5a36534ceed339b5ecd1a
re-sha <163538829+re-sha@users.noreply.github.com>
2024-06-24 02:13:58 +0530

    Navbar and Hero components imported to use here (App component)

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,7 +1,12 @@
 import styles from'./App.module.css'
+import Hero from './components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  return <div className={styles.App}>Hello Resha!</div>;
+  return <div className={styles.App}>
+    <Navbar/>
+    <Hero />
+  </div>;
 }
 
 export default App;
