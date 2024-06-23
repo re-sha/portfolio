0aaf74e70543415a4d200e3bfcb2cffa4e80786c
re-sha <163538829+re-sha@users.noreply.github.com>
2024-06-24 02:35:53 +0530

    Add html for hero component

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -6,19 +6,21 @@ import { getImageFromUrl } from '../../utils';
 
 const Hero = () => {
   return (
-    <section>
-        <div className={styles.heading}>
-            <h1>Hi, Resha Here!</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus modi ut quas unde eveniet rem aperiam. Molestiae pariatur illum nemo corrupti a ea</p>
-            <button>Contact Me</button>
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi, Resha Here!</h1>
+            <p className={styles.description}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus modi ut quas unde eveniet rem aperiam. Molestiae pariatur illum nemo corrupti a ea</p>
+            <button className={styles.contactBtn}>Contact Me</button>
         </div>
         <div>
-            <img src = {getImageFromUrl("hero/heroImage.png")} alt="image of a software developer"></img>
+            <img src = {getImageFromUrl("hero/heroImage.png")} alt="image of a software developer" className={styles.heroImg}></img>
         </div>
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
     </section>
   );
 };
 
-export default Hero
+export default Hero;
 
 
