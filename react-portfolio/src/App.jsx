33751ac4bbcb9e431e162906e4388d729919a1e4
re-sha <163538829+re-sha@users.noreply.github.com>
2024-06-25 07:44:32 +0530

    fix bug of blank screen by importing named exports correctly

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,8 +1,8 @@
 import styles from'./App.module.css'
-import Hero from './components/Hero/Hero';
-import Navbar from './components/Navbar/Navbar';
+import {Hero} from './components/Hero/Hero';
+import {Navbar} from './components/Navbar/Navbar';
 
-function App() {
+export function App() {
   return <div className={styles.App}>
     <Navbar/>
     <Hero />
