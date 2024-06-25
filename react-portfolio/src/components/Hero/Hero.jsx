3227d91e58153c5be2c475a1863257438aa54037
re-sha <163538829+re-sha@users.noreply.github.com>
2024-06-25 07:41:40 +0530

    change from default export to named export

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
-import styles from "../Hero/Hero.module.css"
+import styles from "./Hero.module.css"
 import { getImageFromUrl } from '../../utils';
 
-
-
-const Hero = () => {
+export const Hero = () => {
   return (
     <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, Resha Here!</h1>
-            <p className={styles.description}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus modi ut quas unde eveniet rem aperiam. Molestiae pariatur illum nemo corrupti a ea</p>
-            <button className={styles.contactBtn}>Contact Me</button>
+            <p className={styles.description}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus modi ut quas unde eveniet rem aperiam. Molestiae pariatur illum nemo corrupti a ea
+            </p>
+            <a href = "mailto:email@email.com" className={styles.contactBtn}>
+                Contact Me
+            </a>
         </div>
-        <div>
-            <img src = {getImageFromUrl("hero/heroImage.png")} alt="image of a software developer" className={styles.heroImg}></img>
-        </div>
+        <img src = {getImageFromUrl("hero/heroImage.png")} alt="image of a software developer" className={styles.heroImg}></img>
         <div className={styles.topBlur} />
         <div className={styles.bottomBlur} />
     </section>
   );
 };
 
-export default Hero;
 
 
