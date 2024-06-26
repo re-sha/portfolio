43e5ca85355791a3130673f372944e1f5825cfb9
re-sha <163538829+re-sha@users.noreply.github.com>
2024-06-27 03:31:44 +0530

    import About component into App component

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import styles from'./App.module.css'
-import {Hero} from './components/Hero/Hero';
-import {Navbar} from './components/Navbar/Navbar';
+import { About } from './components/About/About';
+import { Hero } from './components/Hero/Hero';
+import { Navbar } from './components/Navbar/Navbar';
 
 export function App() {
   return <div className={styles.App}>
     <Navbar/>
     <Hero />
+    <About />
   </div>;
 }
 
