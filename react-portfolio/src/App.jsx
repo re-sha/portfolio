462541a6246d7c5e74350a4288faa5f8a7ac9b70
re-sha <163538829+re-sha@users.noreply.github.com>
2024-06-29 04:45:51 +0530

    import experience component to App component

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,13 +1,15 @@
 import styles from'./App.module.css'
 import { About } from './components/About/About';
+import { Experience } from './components/Experience/Experience';
 import { Hero } from './components/Hero/Hero';
 import { Navbar } from './components/Navbar/Navbar';
 
-export function App() {
+function App() {
   return <div className={styles.App}>
     <Navbar/>
     <Hero />
     <About />
+    <Experience />
   </div>;
 }
 
