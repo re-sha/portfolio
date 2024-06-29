6dec1189db4ba0b52a9cb6b63d320b4123837783
re-sha <163538829+re-sha@users.noreply.github.com>
2024-06-30 03:40:07 +0530

    modify syntax of return for readability

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,16 +1,18 @@
-import styles from'./App.module.css'
-import { About } from './components/About/About';
-import { Experience } from './components/Experience/Experience';
-import { Hero } from './components/Hero/Hero';
-import { Navbar } from './components/Navbar/Navbar';
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
-  return <div className={styles.App}>
+  return (
+  <div className={styles.App}>
     <Navbar/>
     <Hero />
     <About />
     <Experience />
-  </div>;
+  </div>
+  );
 }
 
 export default App;
