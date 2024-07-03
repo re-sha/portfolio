f2234e611733b972dc7235824aa3280d66022524
re-sha <163538829+re-sha@users.noreply.github.com>
2024-07-03 11:05:38 +0530

    refactor code and add classnames

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -1,32 +1,15 @@
 import React from "react";
 import projects from "../../data/projects.json";
-import { getImageFromUrl } from "../../utils";
+import { ProjectCard } from "./ProjectCard";
+import styles from "./Projects.module.css"
 
 export const Projects = () => {
   return (
-    <section>
-        <h2>Projects</h2>
-        <div>
+    <section className={styles.container}>
+        <h2 className={styles.title}>Projects</h2>
+        <div className={styles.projects}>
             {projects.map((project, id) => {
-                return (
-                    <div key = {id}>
-                        <img 
-                            src = {getImageFromUrl(project.imageSrc)}
-                            alt= {`Image of ${project.title}`}
-                        />
-                        <h3>{project.title}</h3>
-                        <p>{project.description}</p>
-                        <uL>
-                            {project.skills.map((skill, id) =>{
-                                <li key={id}>{skill}</li>;
-                            })}
-                        </uL>
-                        <div>
-                            <a href={project.demo}>Demo</a>
-                            <a href={project.source}>Source</a>
-                        </div>
-                    </div>           
-                );
+                return <ProjectCard key={id} project={project} />  
             })}
         </div>    
     </section>
