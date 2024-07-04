abe2a5157ba47f2bc7f04f4b0a595a2deefb4f8f
re-sha <163538829+re-sha@users.noreply.github.com>
2024-07-04 07:55:54 +0530

    avoid cache issues with assets

--- react-portfolio/vite.config.js
+++ react-portfolio/vite.config.js
@@ -10,6 +10,13 @@ export default defineConfig({
     },
   },
   build: {
-    cache: false
+    cache: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
   }
 });
